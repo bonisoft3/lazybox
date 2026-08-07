2d690446c8bd5ee7c0ac8bd5f0253f0ebd22940c
davireis <davi@davi.eng.br>
2026-08-06 23:35:55 -0300

    Sync: Merge pull request #1590 from worldsense/feat/pinned-compose-toolchain
    From worldsense/trash
    
    GitOrigin-RevId: 97642fac4806d4dd53721818a5f26da1624122ea

--- docker-stub.sh
+++ docker-stub.sh
@@ -2,12 +2,12 @@
 # Get the directory where this script lives
 DIR="$(dirname "$(readlink -f "$0")")"
 
-mkdir -p $HOME/.docker/cli-plugins/
-if [ ! -x $HOME/.docker/cli-plugins/docker-compose ]; then
- ln -sf $DIR/docker-cli-plugin-docker-compose $HOME/.docker/cli-plugins/docker-compose
+mkdir -p "$HOME/.docker/cli-plugins/"
+if [ ! -x "$HOME/.docker/cli-plugins/docker-compose" ]; then
+ ln -sf "$DIR/docker-cli-plugin-docker-compose" "$HOME/.docker/cli-plugins/docker-compose"
 fi
-if [ ! -x $HOME/.docker/cli-plugins/docker-buildx ]; then
- ln -sf $DIR/docker-cli-plugin-docker-buildx $HOME/.docker/cli-plugins/docker-buildx
+if [ ! -x "$HOME/.docker/cli-plugins/docker-buildx" ]; then
+ ln -sf "$DIR/docker-cli-plugin-docker-buildx" "$HOME/.docker/cli-plugins/docker-buildx"
 fi
 "$DIR/../libexec/lazy-mise" trust -y -a -q .
 MISE_LOCKED=0 exec "$DIR/../libexec/lazy-mise" tool-stub "$DIR/docker.toml" "$@"
